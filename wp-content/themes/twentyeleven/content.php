4833d77e8c9223b8495b44dc06d323e813ca2217
Hirasaki <asutoronoka@gmail.com>
2018-09-16 23:04:41 +0900

    remove comment from toppage

--- wp-content/themes/twentyeleven/content.php
+++ wp-content/themes/twentyeleven/content.php
@@ -25,11 +25,11 @@
 			</div><!-- .entry-meta -->
 			<?php endif; ?>
 
-			<?php if ( comments_open() && ! post_password_required() ) : ?>
-			<div class="comments-link">
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'twentyeleven' ) . '</span>', _x( '1', 'comments number', 'twentyeleven' ), _x( '%', 'comments number', 'twentyeleven' ) ); ?>
-			</div>
-			<?php endif; ?>
+			<?php //if ( comments_open() && ! post_password_required() ) : ?>
+			<?php //<div class="comments-link">?>
+				<?php //comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'twentyeleven' ) . '</span>', _x( '1', 'comments number', 'twentyeleven' ), _x( '%', 'comments number', 'twentyeleven' ) ); ?>
+			<?php //</div>?>
+			<?php //endif; ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
